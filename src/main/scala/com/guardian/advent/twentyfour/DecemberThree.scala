4dca9d8fc68b6dd891cac37cf1e5cef6eff29b94
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2024-12-17 19:17:12 +0000

    wip

--- src/main/scala/com/guardian/advent/twentyfour/DecemberThree.scala
+++ src/main/scala/com/guardian/advent/twentyfour/DecemberThree.scala
@@ -4,6 +4,8 @@ trait DecemberThree extends AdventOfCode with App {
   override def day: Int = 3
   val m = """mul\((\d+),(\d+)\)""".r
 
+  val dont = "don't()"
+  val doo = "do()"
   val input = lineParser[String]() { s => Some(s)}
     println(input.size)
 
@@ -20,7 +22,58 @@ object DecemberThreeOne extends DecemberThree {
 
   val total = input
     .flatMap { s => multsForString(s) }
-    .foldLeft(0){ case(a, b) => a + b}
+    .foldLeft(0){ case(a, b) => a + b }
 
   println(total)
+}
+
+object DecemberThreeTwo extends DecemberThree {
+  def makeNextDelim(delim: String): String = if (delim == dont) doo else dont
+
+  def end(rawInstructions: String) = rawInstructions.indexOf(doo) < 0 && rawInstructions.indexOf(dont) < 0
+
+  def startStop(rawInstructions: String): String =
+    rawInstructions.indexOf(dont) < rawInstructions.indexOf(doo) match {
+      case true => dont
+      case false => doo
+
+    }
+
+  def process(delim: String) = delim == dont
+
+  def makeNextInstructions(rawInstructions: String, delim: String): String = {
+    if (end(rawInstructions)) "" else rawInstructions.substring(rawInstructions.indexOf(delim) + delim.length)
+  }
+
+
+  def findInstructions
+  (rawInstructions: String, delim: String, cleanInstructions: List[(String, Boolean)] = List.empty): List[(String, Boolean)] = {
+
+    def makeListedInstructions: String =
+      if (end(rawInstructions)) rawInstructions else rawInstructions.substring(0, rawInstructions.indexOf(delim))
+
+    if (rawInstructions.isEmpty) cleanInstructions.reverse
+    else {
+      val nextDelim = makeNextDelim(delim)
+      val nextRawInstructions = makeNextInstructions(rawInstructions, delim)
+      findInstructions(
+        nextRawInstructions,
+        nextDelim,
+        (makeListedInstructions(rawInstructions, delim), process(delim)) :: cleanInstructions
+      )
+    }
+  }
+
+  val delim = startStop(rawInstructions)
+  val rawInstructions = input.foldLeft(new StringBuilder()) { case(stringBuilder, string) => stringBuilder.append(string)}.toString()
+  val cleanInstructions = findInstructions(rawInstructions, delim)
+    .flatMap { case(instructions, shouldProcess) => if(shouldProcess) Some(instructions) else None }
+    .foldLeft(new StringBuilder()) { case(stringBuilder: StringBuilder, string: String) => stringBuilder.append(string) }
+    .toString
+
+  val total = multsForString(cleanInstructions)
+    .foldLeft(0L){ case(total, i) => total + i }
+
+  println(total)
+
 }
