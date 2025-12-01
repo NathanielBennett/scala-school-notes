b61e9369bfe51991cc9d9acd8eefffeb4ffc0b82
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2025-12-01 22:30:21 +0000

    take case

--- src/main/test/com/guardian/advent/twentyfive/PuzzleTests.scala
+++ src/main/test/com/guardian/advent/twentyfive/PuzzleTests.scala
@@ -1,5 +1,6 @@
 package com.guardian.advent.twentyfive
 
+import com.guardian.advent.DecemberTwentyFive
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest._
 import flatspec._
@@ -9,13 +10,18 @@ import matchers._
 
 class PuzzleTests extends AnyFlatSpec with should.Matchers {
 
-  "day one" should "return the correct values" in {
-    val testsAndExpected: List[(DecemberOne, Int)] = List(
+  "day one" should "return the correct values" in new TestRunner[Int, List[(String, Int)], Int] {
+    override def testsAndExpected: List[(DecemberOne, Int)] = List(
       (DecemberOnePartOneTest, 3),
       (DecemberOnePartOneSolution, 1165),
       (DecemberOnePartTwoTest, 6),
       (DecemberOnePartTwoSolution, 6496),
     )
+  }
+
+  trait TestRunner[A, B, C] {
+
+    def testsAndExpected: List[(DecemberTwentyFive[A,B,C], A)]
 
     testsAndExpected.foreach{
       case (puzzle, expectedSolution) => puzzle.solve shouldBe expectedSolution
